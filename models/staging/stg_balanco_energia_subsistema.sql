dfe6d2544ee1beba61537c0697536af0a3c14644
Israel Teixeira <israelmteixeira1@gmail.com>
2025-10-06 20:19:45 -0300

    refactor: adapta o model de balancao_energia para pegar dados de 2023 e 2024 também

--- models/staging/stg_balanco_energia_subsistema.sql
+++ models/staging/stg_balanco_energia_subsistema.sql
@@ -1,18 +1,59 @@
 {{ config(schema='STAGING_ONS', materialized='view') }}
 
 with raw as (
-    select *
+    select 
+        VAL_CARGA,
+        DIN_INSTANTE,
+        VAL_GERSOLAR,
+        ID_SUBSISTEMA,
+        VAL_GEREOLICA,
+        NOM_SUBSISTEMA,
+        VAL_GERTERMICA,
+        VAL_INTERCAMBIO,
+        VAL_GERHIDRAULICA,
+        2023 as ano_referencia
+    from {{ source('raw_ons', 'BALANCO_ENERGIA_SUBSISTEMA_2023') }}
+    
+    union all
+    
+    select 
+        VAL_CARGA,
+        DIN_INSTANTE,
+        VAL_GERSOLAR,
+        ID_SUBSISTEMA,
+        VAL_GEREOLICA,
+        NOM_SUBSISTEMA,
+        VAL_GERTERMICA,
+        VAL_INTERCAMBIO,
+        VAL_GERHIDRAULICA,
+        2024 as ano_referencia
+    from {{ source('raw_ons', 'BALANCO_ENERGIA_SUBSISTEMA_2024') }}
+    
+    union all
+    
+    select 
+        VAL_CARGA,
+        DIN_INSTANTE,
+        VAL_GERSOLAR,
+        ID_SUBSISTEMA,
+        VAL_GEREOLICA,
+        NOM_SUBSISTEMA,
+        VAL_GERTERMICA,
+        VAL_INTERCAMBIO,
+        VAL_GERHIDRAULICA,
+        2025 as ano_referencia
     from {{ source('raw_ons', 'BALANCO_ENERGIA_SUBSISTEMA_2025') }}
 )
 
 select
-  VAL_CARGA,
-  DIN_INSTANTE,
-  VAL_GERSOLAR,
-  ID_SUBSISTEMA,
-  VAL_GEREOLICA,
-  NOM_SUBSISTEMA,
-  VAL_GERTERMICA,
-  VAL_INTERCAMBIO,
-  VAL_GERHIDRAULICA
+    VAL_CARGA,
+    DIN_INSTANTE,
+    VAL_GERSOLAR,
+    ID_SUBSISTEMA,
+    VAL_GEREOLICA,
+    NOM_SUBSISTEMA,
+    VAL_GERTERMICA,
+    VAL_INTERCAMBIO,
+    VAL_GERHIDRAULICA,
+    ano_referencia
 from raw
