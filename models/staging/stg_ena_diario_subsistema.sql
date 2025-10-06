87280d3a8b605e35971fbd5b72064400b7654bd6
Israel Teixeira <israelmteixeira1@gmail.com>
2025-10-06 20:33:38 -0300

    refactor: adapta o model de ena_diario_subsistemas para pegar dados de 2023 e 2024 também

--- models/staging/stg_ena_diario_subsistema.sql
+++ models/staging/stg_ena_diario_subsistema.sql
@@ -1,16 +1,51 @@
 {{ config(schema='STAGING_ONS', materialized='view') }}
 
 with raw as (
-    select *
+    select 
+        ENA_DATA,
+        ID_SUBSISTEMA,
+        NOM_SUBSISTEMA,
+        ENA_BRUTA_REGIAO_MWMED,
+        ENA_ARMAZENAVEL_REGIAO_MWMED,
+        ENA_BRUTA_REGIAO_PERCENTUALMLT,
+        ENA_ARMAZENAVEL_REGIAO_PERCENTUALMLT,
+        2023 as ano_referencia
+    from {{ source('raw_ons', 'ENA_DIARIO_SUBSISTEMA_2023') }}
+    
+    union all
+    
+    select 
+        ENA_DATA,
+        ID_SUBSISTEMA,
+        NOM_SUBSISTEMA,
+        ENA_BRUTA_REGIAO_MWMED,
+        ENA_ARMAZENAVEL_REGIAO_MWMED,
+        ENA_BRUTA_REGIAO_PERCENTUALMLT,
+        ENA_ARMAZENAVEL_REGIAO_PERCENTUALMLT,
+        2024 as ano_referencia
+    from {{ source('raw_ons', 'ENA_DIARIO_SUBSISTEMA_2024') }}
+    
+    union all
+    
+    select 
+        ENA_DATA,
+        ID_SUBSISTEMA,
+        NOM_SUBSISTEMA,
+        ENA_BRUTA_REGIAO_MWMED,
+        ENA_ARMAZENAVEL_REGIAO_MWMED,
+        ENA_BRUTA_REGIAO_PERCENTUALMLT,
+        ENA_ARMAZENAVEL_REGIAO_PERCENTUALMLT,
+        2025 as ano_referencia
     from {{ source('raw_ons', 'ENA_DIARIO_SUBSISTEMA_2025') }}
 )
 
 select
-  ENA_DATA,
-  ID_SUBSISTEMA,
-  NOM_SUBSISTEMA,
-  ENA_BRUTA_REGIAO_MWMED,
-  ENA_ARMAZENAVEL_REGIAO_MWMED,
-  ENA_BRUTA_REGIAO_PERCENTUALMLT,
-  ENA_ARMAZENAVEL_REGIAO_PERCENTUALMLT
+    ENA_DATA,
+    ID_SUBSISTEMA,
+    NOM_SUBSISTEMA,
+    ENA_BRUTA_REGIAO_MWMED,
+    ENA_ARMAZENAVEL_REGIAO_MWMED,
+    ENA_BRUTA_REGIAO_PERCENTUALMLT,
+    ENA_ARMAZENAVEL_REGIAO_PERCENTUALMLT,
+    ano_referencia
 from raw
