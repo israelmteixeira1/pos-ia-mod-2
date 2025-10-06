475d87b99d8e976dbb4b7aa725cb0ec3b50acb60
Israel Teixeira <israelmteixeira1@gmail.com>
2025-10-06 20:30:10 -0300

    refactor: adapta o model de ear_diario_subsistemas para pegar dados de 2023 e 2024 também

--- models/staging/stg_ear_diario_subsistema.sql
+++ models/staging/stg_ear_diario_subsistema.sql
@@ -1,15 +1,47 @@
 {{ config(schema='STAGING_ONS', materialized='view') }}
 
 with raw as (
-    select *
+    select 
+        EAR_DATA,
+        ID_SUBSISTEMA,
+        NOM_SUBSISTEMA,
+        EAR_MAX_SUBSISTEMA,
+        EAR_VERIF_SUBSISTEMA_MWMES,
+        EAR_VERIF_SUBSISTEMA_PERCENTUAL,
+        2023 as ano_referencia
+    from {{ source('raw_ons', 'EAR_DIARIO_SUBSISTEMA_2023') }}
+    
+    union all
+    
+    select 
+        EAR_DATA,
+        ID_SUBSISTEMA,
+        NOM_SUBSISTEMA,
+        EAR_MAX_SUBSISTEMA,
+        EAR_VERIF_SUBSISTEMA_MWMES,
+        EAR_VERIF_SUBSISTEMA_PERCENTUAL,
+        2024 as ano_referencia
+    from {{ source('raw_ons', 'EAR_DIARIO_SUBSISTEMA_2024') }}
+    
+    union all
+    
+    select 
+        EAR_DATA,
+        ID_SUBSISTEMA,
+        NOM_SUBSISTEMA,
+        EAR_MAX_SUBSISTEMA,
+        EAR_VERIF_SUBSISTEMA_MWMES,
+        EAR_VERIF_SUBSISTEMA_PERCENTUAL,
+        2025 as ano_referencia
     from {{ source('raw_ons', 'EAR_DIARIO_SUBSISTEMA_2025') }}
 )
 
 select
-  EAR_DATA,
-  ID_SUBSISTEMA,
-  NOM_SUBSISTEMA,
-  EAR_MAX_SUBSISTEMA,
-  EAR_VERIF_SUBSISTEMA_MWMES,
-  EAR_VERIF_SUBSISTEMA_PERCENTUAL
+    EAR_DATA,
+    ID_SUBSISTEMA,
+    NOM_SUBSISTEMA,
+    EAR_MAX_SUBSISTEMA,
+    EAR_VERIF_SUBSISTEMA_MWMES,
+    EAR_VERIF_SUBSISTEMA_PERCENTUAL,
+    ano_referencia
 from raw
